88389db18fbfd072b493ac763d02bf6ff1043f88
NS1010 <47858774+NS1010@users.noreply.github.com>
2019-05-17 17:59:42 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log("Hello World");
+alert("Hello!");
+
+var target = document.getElementById("target");
+target.innerText  = "Hi!";
+
+var button = document.getElementById("button");
+button.addEventListener('click', function () {
+  alert("yes thanks for clicking me");
+  var username = document.getElementById("username");
+  is (username.value == "admin") {
+      alert("Welcome!");
+  } else {
+      alert("go away you are not welcome >:(")
+  }
+});
